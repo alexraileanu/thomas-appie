d1692272f9c7c9468305fea458b1b01b7d079259
alexraileanu <alexraileanu@me.com>
2023-02-22 10:46:33 +0100

    feat: initial commit; sends messages to discord on startup

---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+type shield struct {
+	Theme string
+	Text  string
+}
+
+type discount struct {
+	Theme string
+	Start string `json:"startDate"`
+	End   string `json:"endDate"`
+}
+
+type productResponse struct {
+	Cards []struct {
+		Products []struct {
+			Shield   shield   `json:"shield"`
+			Discount discount `json:"discount"`
+		} `json:"products"`
+	} `json:"cards"`
+}
+
+const AppieURL = "https://www.ah.nl/zoeken/api/products/taxonomy-brand?brand=Perla%20Superiore"
+
+//const AppieURL = "https://www.ah.nl/zoeken/api/products/taxonomy-brand?brand=Perla%20Huisblends"
+
+func main() {
+	resp, err := makeRequest()
+	if err != nil {
+		panic(err)
+	}
+
+	response := new(productResponse)
+	err = parseBody(resp, &response)
+	if err != nil {
+		panic(err)
+	}
+
+	product := response.Cards[0].Products[0]
+	hasShield := product.Shield != shield{}
+	hasDiscount := product.Discount != discount{}
+
+	msg := "Beans not in bonus :("
+	if hasShield && hasDiscount {
+		msg = fmt.Sprintf(`
+Beans are in bonus!
+
+%s (starts: %s; ends: %s)
+`, product.Shield.Text, product.Discount.Start, product.Discount.End)
+	}
+
+	thomas := initThomas()
+	thomas.ChannelMessageSend(os.Getenv("DISCORD_CHANNEL_ID"), msg)
+
+	handleClose(thomas)
+}
+
+func makeRequest() (*http.Response, error) {
+	req, err := http.NewRequest("GET", AppieURL, bytes.NewBuffer(nil))
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func initThomas() *discordgo.Session {
+	thomas, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = thomas.Open()
+	if err != nil {
+		fmt.Println("Error opening Discord session: ", err)
+	}
+
+	return thomas
+}
+
+func handleClose(thomas *discordgo.Session) {
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Thomas is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	thomas.Close()
+}
+
+func parseBody(resp *http.Response, target any) error {
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	err = resp.Body.Close()
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(responseBody, target)
+}
