b0bd14f1c449297ff6d11a51d2235de5f14a0a9d
alexraileanu <alexraileanu@me.com>
2023-02-22 11:10:37 +0100

    feat: added some explanatory comments

--- main.go
+++ main.go
@@ -41,10 +41,12 @@ const AppieURL = "https://www.ah.nl/zoeken/api/products/taxonomy-brand?brand=Per
 //const AppieURL = "https://www.ah.nl/zoeken/api/products/taxonomy-brand?brand=Perla%20Huisblends"
 
 func main() {
-
 	thomas := initThomas()
 
 	s := gocron.NewScheduler(time.Local)
+
+	// scheduler that runs every day at 10AM (for now for debug purposes only)
+	// eventually it will run every monday
 	s.Every(1).Day().At("10:30").Do(func() {
 		goThomasGo(thomas)
 	})
@@ -52,6 +54,7 @@ func main() {
 	handleClose(thomas)
 }
 
+// makeRequest gets the product info from the API
 func makeRequest() (*http.Response, error) {
 	req, err := http.NewRequest("GET", AppieURL, bytes.NewBuffer(nil))
 	if err != nil {
@@ -67,6 +70,7 @@ func makeRequest() (*http.Response, error) {
 	return resp, nil
 }
 
+// initThomas makes the initial connection to discord
 func initThomas() *discordgo.Session {
 	thomas, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
 	if err != nil {
@@ -81,17 +85,7 @@ func initThomas() *discordgo.Session {
 	return thomas
 }
 
-func handleClose(thomas *discordgo.Session) {
-	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Thomas is now running.  Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
-
-	// Cleanly close down the Discord session.
-	thomas.Close()
-}
-
+// parseBody parses the response body into the given target
 func parseBody(resp *http.Response, target any) error {
 	responseBody, err := io.ReadAll(resp.Body)
 	if err != nil {
@@ -105,6 +99,19 @@ func parseBody(resp *http.Response, target any) error {
 	return json.Unmarshal(responseBody, target)
 }
 
+// handleClose cleanly disconnects and shuts thomas down
+func handleClose(thomas *discordgo.Session) {
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Thomas is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	thomas.Close()
+}
+
+// goThomasGo fetches the info from the API and instructs thomas to send the discord message
 func goThomasGo(thomas *discordgo.Session) {
 	resp, err := makeRequest()
 	if err != nil {
@@ -118,6 +125,10 @@ func goThomasGo(thomas *discordgo.Session) {
 	}
 
 	product := response.Cards[0].Products[0]
+
+	// shield and discount hold the actual discount info
+	// the properties aren't actually present if the product is not discounted
+	// so we check if the shield/discount values are equal to their respective empty structs
 	hasShield := product.Shield != shield{}
 	hasDiscount := product.Discount != discount{}
 
