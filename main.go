c15e37883ac83da3ce8b298f445b0ce62c58ed54
Alex <alexraileanu@me.com>
2025-07-07 09:00:24 +0200

    feat: re-enable thomas

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"os"
+	"time"
+
+	"github.com/go-co-op/gocron"
 
 	"github.com/alexraileanu/thomas-appie/pkg/config"
 	"github.com/alexraileanu/thomas-appie/pkg/logger"
 	"github.com/alexraileanu/thomas-appie/pkg/product"
+	"github.com/alexraileanu/thomas-appie/pkg/thomas"
+	"github.com/alexraileanu/thomas-appie/pkg/utl"
 
 	"github.com/joho/godotenv"
 
@@ -25,10 +30,10 @@ func main() {
 		panic(err)
 	}
 
-	//loggerService.Info("Starting thomas", nil)
-	//s := gocron.NewScheduler(time.Local)
+	loggerService.Info("Starting thomas", nil)
+	s := gocron.NewScheduler(time.Local)
 
-	//loggerService.Info("Connecting to the db", nil)
+	loggerService.Info("Connecting to the db", nil)
 	dbConnection, err := db.New(os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
 	if err != nil {
 		//loggerService.Error("Error connecting to the db", map[string]interface{}{"error": err.Error()})
@@ -36,37 +41,35 @@ func main() {
 	}
 	dbService := db.NewDBService(dbConnection, loggerService)
 
-	//t, err := thomas.New(dbService, loggerService, conf)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//loggerService.Info("Starting cron job", map[string]interface{}{"cron": conf.Thomas.Cron})
-	//s.Cron(conf.Thomas.Cron).Do(func() {
-	//	loggerService.Info("Fetching products from the Appie", nil)
-	//	productsToWatch, err := utl.ParseProductsJson()
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//	t.Go()
-	//
-	//	loggerService.Info("Saving products to the db", map[string]interface{}{"products": productsToWatch})
-	//	err = dbService.SaveProduct(productsToWatch)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//})
-	//s.StartAsync()
+	t, err := thomas.New(dbService, loggerService, conf)
+	if err != nil {
+		panic(err)
+	}
 
-	print("yo")
+	loggerService.Info("Starting cron job", map[string]interface{}{"cron": conf.Thomas.Cron})
+	s.Cron(conf.Thomas.Cron).Do(func() {
+		loggerService.Info("Fetching products from the Appie", nil)
+		productsToWatch, err := utl.ParseProductsJson()
+		if err != nil {
+			panic(err)
+		}
+		t.Go()
+
+		loggerService.Info("Saving products to the db", map[string]interface{}{"products": productsToWatch})
+		err = dbService.SaveProduct(productsToWatch)
+		if err != nil {
+			panic(err)
+		}
+	})
+	s.StartAsync()
 
 	productService := product.New()
 
-	//go func() {
-	loggerService.Info("Starting http server", nil)
-	h := http.NewServer(dbService, productService)
-	h.Start()
-	//}()
+	go func() {
+		loggerService.Info("Starting http server", nil)
+		h := http.NewServer(dbService, productService)
+		h.Start()
+	}()
 
-	//t.Close()
+	t.Close()
 }
